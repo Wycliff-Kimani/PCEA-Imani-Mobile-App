2d54e147ae36af6625a90c1db63859a1c641fdea
Wycliff-Kimani <kimsjackson9@gmail.com>
2026-02-20 08:34:45 +0300

    fix issues 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,14 +62,9 @@ dependencies {
     // create a navigation dependency
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.material3)
-    implementation(libs.font.awesome.v110)
-    implementation(libs.ui)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 
+    implementation(libs.font.awesome.icons)
+
+    implementation(libs.ui) // Very questionable
+    debugImplementation(libs.androidx.ui.tooling)
 }
