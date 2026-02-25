020a78dd022afcaa50212a2ae7753df728a7ad37
Festus <beckyneila4@gmail.com>
2026-02-26 01:01:23 +0300

    Optimize imports

--- app/src/main/java/com/devcraft/pceaimani/ui/components/VerseTodayCard.kt
+++ app/src/main/java/com/devcraft/pceaimani/ui/components/VerseTodayCard.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.devcraft.pceaimani.R
 import com.devcraft.pceaimani.ui.theme.White
 
