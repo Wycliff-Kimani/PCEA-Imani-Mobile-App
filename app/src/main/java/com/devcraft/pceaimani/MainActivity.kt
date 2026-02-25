6f5093d73c42a7488887b0724e7c6c1fac2fb170
Festus <beckyneila4@gmail.com>
2026-02-26 00:44:05 +0300

    Optimize imports

--- app/src/main/java/com/devcraft/pceaimani/MainActivity.kt
+++ app/src/main/java/com/devcraft/pceaimani/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.devcraft.pceaimani.ui.components.BottomNavigationBar
 import com.devcraft.pceaimani.ui.navigation.AppNavGraph
