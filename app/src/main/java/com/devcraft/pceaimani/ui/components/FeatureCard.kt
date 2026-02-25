1620755ad14d86bd79e7dd26640f520dcd10d12b
Festus <beckyneila4@gmail.com>
2026-02-25 23:47:37 +0300

    Optimize imports

--- app/src/main/java/com/devcraft/pceaimani/ui/components/FeatureCard.kt
+++ app/src/main/java/com/devcraft/pceaimani/ui/components/FeatureCard.kt
@@ -1,6 +1,7 @@
 package com.devcraft.pceaimani.ui.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
